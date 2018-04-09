a63d603995578443fe39476cec793db683350040
Tin Kotvas <tin.kotvas@gmail.com>
2018-04-09 15:06:21 +0200

    made a base post.json and a component

---
+++ src/components/PromotedStage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-12 border">
+        <div v-for="post in promotedPosts" :key="post">
+            <h1>{{post}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+const json = require('../assets/json/posts.json')
+
+export default {
+  name: 'PromotedStage',
+  data: {
+      promotedPosts: json
+  }
+}
+</script>
+
+<style>
+
+</style>
