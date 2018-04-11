3694675a92770b77458b0519dbe95af9776eb6ee
Tin Kotvas <tin.kotvas@gmail.com>
2018-04-11 13:12:46 +0200

    added vue-router for SPA

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
 import App from './App'
 
 Vue.use(Buefy)
+Vue.use(VueRouter)
 
 require('../node_modules/bootstrap/scss/bootstrap.scss')
 
