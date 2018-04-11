6fcb201ffff3894f04a8e7e472f1bd563849aad6
Dacrol <jonas.ahrens@gmail.com>
2018-04-11 11:12:34 +0200

    Remove Vuetify, add Buefy

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 import App from './App'
 
-Vue.use(Vuetify)
+Vue.use(Buefy)
 
-require('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons')
-require('../node_modules/vuetify/dist/vuetify.min.css')
 require('../node_modules/bootstrap/scss/bootstrap.scss')
 
 Vue.config.productionTip = false
