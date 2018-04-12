b88f089efdb6c959da8b924cf2978986acf640a1
Dacrol <jonas.ahrens@gmail.com>
2018-04-12 10:46:15 +0200

    Trigger toggle methods on click

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,14 +4,14 @@
     <a class="navbar-item" href="https://bulma.io">
       <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
     </a>
-    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+    <div class="navbar-burger burger" data-target="navbarExampleTransparentExample" @click="toggleMenu" :class="{'is-active': navActive}">
       <span></span>
       <span></span>
       <span></span>
     </div>
   </div>
 
-  <div id="navbarExampleTransparentExample" class="navbar-menu">
+  <div id="navbarExampleTransparentExample" class="navbar-menu" :class="{'is-active': navActive}">
     <div class="navbar-start">
           <router-link class="navbar-item" to="/">Home</router-link>
     <router-link class="navbar-item" to="posts">Posts</router-link>
