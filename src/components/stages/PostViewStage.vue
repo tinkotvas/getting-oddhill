d6ab07c981750f2473db8f739b3b468d24e4804f
Tin Kotvas <tin.kotvas@gmail.com>
2018-04-17 12:20:28 +0200

    pre-eslintfix

--- src/components/stages/PostViewStage.vue
+++ src/components/stages/PostViewStage.vue
@@ -1,21 +1,25 @@
 <template>
-    <b-notification ref="poststage" :closable="false">
-        <div  v-if="post.heading" class="columns is-centerered no-overflow">
-            <div class="column is-10">
-                <article class="media">
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                            <strong>{{post.heading}}</strong> <small>@{{post.author}}</small>
-                            <br><br>
-                            {{post.message}}
-                            </p>
-                        </div>
-                    </div>
-                </article>
+  <b-notification
+    ref="poststage"
+    :closable="false">
+    <div
+      v-if="post.heading"
+      class="columns is-centerered no-overflow">
+      <div class="column is-10">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>{{ post.heading }}</strong> <small>@{{ post.author }}</small>
+                <br><br>
+                {{ post.message }}
+              </p>
             </div>
-        </div>
-    </b-notification>
+          </div>
+        </article>
+      </div>
+    </div>
+  </b-notification>
 </template>
 
 <script>
@@ -24,31 +28,30 @@ import { db } from '../../main.js'
 export default {
   data () {
     return {
-        isLoading: true,
-        post: []
+      isLoading: true,
+      post: []
     }
   },
   mounted () {
-      this.initLoading()
-      this.bindPost()
-      
+    this.initLoading()
+    this.bindPost()
   },
   methods: {
-    bindPost: function(){
-        this.$bind('post', db.collection('posts').doc(this.$route.params.id))
-      .then((doc) => {
-        this.isLoading = false
-        this.loadingComponent.close()
-      })
-      .catch((error) => {
-        console.log('error in loading: ', error)
-      })
+    bindPost: function () {
+      this.$bind('post', db.collection('posts').doc(this.$route.params.id))
+        .then((doc) => {
+          this.isLoading = false
+          this.loadingComponent.close()
+        })
+        .catch((error) => {
+          console.log('error in loading: ', error)
+        })
     },
     initLoading () {
       this.loadingComponent = this.$loading.open({
         container: this.$refs.poststage.$el
       })
-    },
+    }
   }
 }
 
