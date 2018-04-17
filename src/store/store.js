80ed139c54412a6ebe6173248d886c458f56991c
Dacrol <jonas.ahrens@gmail.com>
2018-04-17 13:51:21 +0200

    Add User store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import Vuex from 'vuex'
+import user from './user/user'
 
 const store = new Vuex.Store({
   modules: {
+    user: user
   }
 })
 
---
+++ src/store/user/user.js
@@ -0,0 +1,64 @@
+import * as firebase from 'firebase'
+
+export default {
+  state: {
+    user: null
+  },
+  mutations: {
+    setUser (state, payload) {
+      state.user = payload
+    }
+  },
+  actions: {
+    signUp ({commit}, payload) {
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            const newUser = {
+              id: user.uid,
+              name: user.displayName,
+              email: user.email,
+              photoUrl: user.photoURL
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(
+          error => {
+            console.log(error)
+          }
+        )
+    },
+    signIn ({commit}, payload) {
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            const newUser = {
+              id: user.uid,
+              name: user.displayName,
+              email: user.email,
+              photoUrl: user.photoURL
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(
+          error => {
+            console.log(error)
+          }
+        )
+    },
+    autoSignIn ({commit}, payload) {
+      commit('setUser', {
+        id: payload.uid,
+        name: payload.displayName,
+        email: payload.email,
+        photoUrl: payload.photoURL
+      })
+    },
+    signOut ({commit}) {
+      firebase.auth().signOut()
+      commit('setUser', null)
+    }
+  }
+}
