3bad52f10615bd8d41600437bf64dd5d072d69d1
Tin <tin.kotvas@gmail.com>
2018-04-18 23:10:34 +0200

    added getting multiple posts

--- src/store/post/post.js
+++ src/store/post/post.js
@@ -18,6 +18,23 @@ export default {
         }).catch(err => {
           console.log('Error getting documents', err)
         })
+    },
+    getPosts ({ commit }, payload = {}) {
+      if (!payload.orderBy) { payload.orderBy = 'createdAt' }
+      if (!payload.orderIn) { payload.orderIn = 'desc' }
+      if (!payload.limit) { payload.limit = 10 }
+      db.collection('posts').orderBy(payload.orderBy, payload.orderIn).limit(payload.limit).get()
+        .then(snapshot => {
+          let posts = []
+          snapshot.forEach(doc => {
+            let tmp = doc.data()
+            tmp.id = doc.id
+            posts.push(tmp)
+          })
+          commit('setPosts', posts)
+        }).catch(err => {
+          console.log('Error getting documents', err)
+        })
     }
   },
   getters: {
