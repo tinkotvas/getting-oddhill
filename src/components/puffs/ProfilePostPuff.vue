cd38af1da1a12f790f552736f2fa7af00fcb6d3b
Shamin Krook <shahmineh.gh@hotmail.com>
2018-04-19 15:38:01 +0200

    Fixed readble sv time

--- src/components/puffs/ProfilePostPuff.vue
+++ src/components/puffs/ProfilePostPuff.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="columns is-centered">
     <div class="column is-8">
-      <b-notification class="content" :closable="false" ref="ProfilePostPuff">
+      <div
+        class="content"
+        :closable="false"
+        ref="ProfilePostPuff">
         <h3><strong>Inlägg av Batman</strong></h3>
-        <p :v-if="userData" v-for="(post, key) in userData.posts" :key="key">
-            <strong>{{post.heading}}</strong>
-            {{post.createdAt}}
-            <br>{{ (post.message).substring(0, 300) }}... ...
-        </p>
-      </b-notification>
+        <div class="columns is-multiline">
+          <div  class="column is-6"
+            :v-if="userData.posts"
+            v-for="(post, key) in userData.posts"
+            :key="key">
+            <div class="box">
+              <span v-if="post.message">
+                <strong>{{ post.heading }}</strong>
+                <br><span>{{ localTimeSv(post.createdAt) }}</span>
+                <br>{{ post.message | truncate }}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +28,18 @@
 <script>
 
 export default {
-  props: ['userData']
+  filters: {
+    truncate: function (value) {
+      return value.substring(0, 300) + '...'
+    }
+  },
+  props: ['userData'],
+  methods: {
+    localTimeSv: function (value) {
+      let date = this.$moment(value)
+      return date.locale('sv').format('dddd, MMMM Do YYYY')
+    }
+  }
 }
 
 </script>
