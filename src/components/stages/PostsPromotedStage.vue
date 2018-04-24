5f95805dce0d9d8e407939cfe1eda3d0a557ade9
Tin <tin.kotvas@gmail.com>
2018-04-24 12:38:57 +0200

    added router-link to promoted page headings

--- src/components/stages/PostsPromotedStage.vue
+++ src/components/stages/PostsPromotedStage.vue
@@ -8,7 +8,7 @@
       <!--Main content -->
       <div class="media-content no-overflow">
         <div class="content">
-          <p><strong>{{ post.heading }}</strong><br>
+          <p><strong><router-link :to="'/posts/'+post.id">{{ post.heading }}</router-link></strong><br>
             {{ (post.message).substring(0,155) }}...</p>
         </div>
         <nav class="level is-mobile">
