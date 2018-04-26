a955fd5f8a97f8c2861fd73265a4eaf3b5ea0648
PaulinaHT <paulina.hertor@gmail.com>
2018-04-26 12:14:03 +0200

    View-styling in progress

--- src/components/views/PostViewA.vue
+++ src/components/views/PostViewA.vue
@@ -1,45 +1,40 @@
 <template>
   <div class="posts">
-    <section class="section">
-      <div class="container">
-        <div class="column">
-          <div
-            id="pages"
-            class="column">
-            <div id="posts">
-              <article
-                class="media is-loading"
-                v-for="(post, key) of posts"
-                :key="key">
-                <!--Left content like img-->
-                <!--Main content -->
-                <div class="media-content no-overflow">
-                  <div class="content">
-                    <p><strong>{{ post.heading }}</strong><br>
-                      {{ (post.message).substring(0,155) }}...</p>
-                  </div>
-                  <nav class="level is-mobile">
-                    <div class="level-left"/>
-                    <div class="level-right">
-                      <b-taglist>
-                        <router-link
-                          v-for="(topic,key) of post.topics"
-                          :key="key"
-                          class="is-info is-small tag"
-                          :to="'topic?'+topic">
-                          {{ topic }}
-                        </router-link>
-                      </b-taglist>
-                    </div>
-                  </nav>
-                </div>
-              </article>
-            </div>
+    <div
+      id="pages"
+      class="column">
+      <div id="posts">
 
+
+        <article
+          class="media is-loading"
+          v-for="(post, key) of posts"
+          :key="key">
+          <div class="media-content no-overflow">
+            <div class="content">
+              <p><strong>{{ post.heading }}</strong><br>
+                {{ (post.message).substring(0,155) }}...</p>
+            </div>
+            <nav class="level is-mobile">
+              <div class="level-left"/>
+              <div class="level-right">
+                <b-taglist>
+                  <router-link
+                    v-for="(topic,key) of post.topics"
+                    :key="key"
+                    class="is-info is-small tag"
+                    :to="'topic?'+topic">
+                    {{ topic }}
+                  </router-link>
+                </b-taglist>
+              </div>
+            </nav>
           </div>
-        </div>
+        </article>
+
+
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -60,53 +55,3 @@ export default {
   }
 }
 </script>
-
-<!--<template>
-  <div class="posts">
-    <section class="section">
-      <div class="container">
-         <div class="container">
-          <div
-            class="view-btn field is-grouped"
-            align="right">
-            <p class="control">
-              <a
-                class="button is-primary"
-                @click="postsView='post-view-a'">View A</a>
-              <a
-                class="button is-primary"
-                @click="postsView='post-view-b'">View B</a>
-              <a
-                class="button is-primary"
-                @click="postsView='post-view-c'">View C</a>
-            </p>
-          </div>
-        </div>
-        <keep-alive>
-          <component :is="postsPage">
-            <div class="columns">
-              <div
-                id="posts"
-                class="column">
-                <posts-page/>
-              </div>
-            </div>
-          </component>
-        </keep-alive>
-      </div>
-    </section>
-  </div>
-</template> -->
-
-<!-- <script>
-// // Stages
-// import PostsPage from '../pages/PostsPage'
-
-// export default {
-//   components: {
-//     PostsPage
-//   }
-// }
-// </script>
-
-
--- src/components/views/PostViewC.vue
+++ src/components/views/PostViewC.vue
@@ -1,45 +1,41 @@
 <template>
   <div class="posts">
-    <section class="section">
-      <div class="container">
-        <div class="column">
-          <div
-            id="stages"
-            class="column"
-            style="color: blue">
-             <div id="posts">
-              <article
-                class="media is-loading"
-                v-for="(post, key) of posts"
-                :key="key">
-                <!--Left content like img-->
-                <!--Main content -->
-                <div class="media-content no-overflow">
-                  <div class="content">
-                    <p><strong>{{ post.heading }}</strong><br>
-                      {{ (post.message).substring(0,155) }}...</p>
-                  </div>
-                  <nav class="level is-mobile">
-                    <div class="level-left"/>
-                    <div class="level-right">
-                      <b-taglist>
-                        <router-link
-                          v-for="(topic,key) of post.topics"
-                          :key="key"
-                          class="is-info is-small tag"
-                          :to="'topic?'+topic">
-                          {{ topic }}
-                        </router-link>
-                      </b-taglist>
-                    </div>
-                  </nav>
-                </div>
-              </article>
+    <div
+      id="stages"
+      class="column"
+      style="color: blue">
+      <div id="posts">
+
+
+        <article
+          class="media is-loading"
+          v-for="(post, key) of posts"
+          :key="key">
+          <div class="media-content no-overflow">
+            <div class="content">
+              <p><strong>{{ post.heading }}</strong><br>
+                {{ (post.message).substring(0,155) }}...</p>
             </div>
+            <nav class="level is-mobile">
+              <div class="level-left"/>
+              <div class="level-right">
+                <b-taglist>
+                  <router-link
+                    v-for="(topic,key) of post.topics"
+                    :key="key"
+                    class="is-info is-small tag"
+                    :to="'topic?'+topic">
+                    {{ topic }}
+                  </router-link>
+                </b-taglist>
+              </div>
+            </nav>
           </div>
-        </div>
+        </article>
+
+
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
