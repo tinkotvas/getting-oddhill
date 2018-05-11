72436aa3996758a37679400102cce83fc291f1b0
PaulinaHT <paulina.hertor@gmail.com>
2018-05-11 15:21:39 +0200

    Views are responsive

--- src/components/views/PostViewA.vue
+++ src/components/views/PostViewA.vue
@@ -1,68 +1,47 @@
 <template>
-<div class="section">
   <div class="columns is-centered">
-    <div class="column is-9">
-        <div class="posts">
-          <div
-            id="pages"
-            class="column">
-            <div id="posts">
-
-              <article
-                class="level media is-loading"
-                v-for="(post, key) of posts"
-                :key="key">
-
-                <div class="media-content no-overflow">
-                    <div class="content">
-                      <h4><strong><router-link :to="'/post/'+ post.id">{{ post.heading }}</router-link></strong><br>
-                        <span><small>{{ localTimeSv(post.createdAt.toDate()) }}</small></span>
-                      <span><small>{{ post.author.username || 'Anonym' }}</small></span></h4>
-                      <router-link :to="'/post/'+post.id">
-                        <p> {{ post.message | truncate }}</p>
-                      </router-link>
-                    </div>
-                  </div>
-                  <div class="media-right">
-                    <div class="level">
-                      <div class="level-left"/>
-                      <div class="level-right">
-                        <figure class="img is-128x128">
-                          <img
-                            :src="post.imageUrl"
-                            v-if="post.imageUrl">
-                        </figure>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="level-item">
-                   <nav
-                      class="level is-mobile"
-                      style="margin-top:30px">
-                      <!-- <div class="level-left">
-                        <router-link :to="'/post/'+ post.id"><small><a> {{ 4 }} kommentarer</a></small></router-link>
-                      </div> -->
-                      <div class="level-right">
-                      <b-taglist>
-                        <router-link
-                          v-for="(topic,key) of post.topics"
-                          :key="key"
-                          class="is-info is-small tag"
-                          :to="'topic?'+topic">
-                          {{ topic }}
-                        </router-link>
-                       </b-taglist>
-                      </div>
-                    </nav>
-                  </div>
-              </article>
-
-
-            </div>
+    <div class="column is-10-desktop">
+      <article
+        class="media"
+        v-for="(post, key) of posts"
+        :key="key">
+        <div class="media-content no-overflow">
+          <div class="content">
+            <h4><strong><router-link :to="'/post/'+ post.id">{{ post.heading }}</router-link></strong></h4><br>
+              <span class="time-author"><small>{{ localTimeSv(post.createdAt.toDate()) }}<small>av</small>
+            {{ post.author.username || 'Anonym' }}</small></span>
+            <router-link :to="'/post/'+post.id">
+              <p> {{ post.message | truncate }}</p>
+            </router-link>
           </div>
+          <!-- <figure class="img media-right is-128x128">
+            <img
+              :src="post.imageUrl"
+              v-if="post.imageUrl">
+          </figure> -->
+          <nav class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <router-link :to="'/post/'+ post.id"><small><a> {{ 4 }} kommentarer</a></small></router-link>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <b-taglist>
+                  <router-link
+                    v-for="(topic,key) of post.topics"
+                    :key="key"
+                    class="is-light is-small tag"
+                    :to="'/topic/'+ post.topics">
+                    {{ topic }}
+                  </router-link>
+                </b-taglist>
+              </div>
+            </div>
+          </nav>
         </div>
+      </article>
 
-      </div>
     </div>
   </div>
 </template>
@@ -98,68 +77,6 @@ export default {
 }
 </script>
 
-<!--<template>
-  <div class="posts">
-    <section class="section">
-      <div class="container">
-         <div class="container">
-          <div
-            class="view-btn field is-grouped"
-            align="right">
-            <p class="control">
-              <a
-                class="button is-primary"
-                @click="postsView='post-view-a'">View A</a>
-              <a
-                class="button is-primary"
-                @click="postsView='post-view-b'">View B</a>
-              <a
-                class="button is-primary"
-                @click="postsView='post-view-c'">View C</a>
-            </p>
-          </div>
-        </div>
-        <keep-alive>
-          <component :is="postsPage">
-            <div class="columns">
-              <div
-                id="posts"
-                class="column">
-                <posts-page/>
-              </div>
-            </div>
-          </component>
-        </keep-alive>
-      </div>
-    </section>
-  </div>
-</template> -->
-
-.profile-box {
- background-color: rgba(152, 226, 248, 0.212);
-}
-img {
-  height: 200px;
-  width: auto;
-  float: right;
-}
-
-.level-item{
-  align-self: flex-end;
-  justify-content: flex-start;
-
-}
-</style>
-
-// export default {
-//   components: {
-//     PostsPage
-//   }
-// }
-// </script>
-
-
-
 <style scoped>
 
 .profile-box {
@@ -174,6 +91,9 @@ img {
 .level-item{
   align-self: flex-end;
   justify-content: flex-start;
+}
 
+.time-author{
+  color: rgba(58,58,58);
 }
 </style>
