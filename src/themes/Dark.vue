5b6e3c47a21977f62c99412485a15a17a7918efd
Dacrol <jonas.ahrens@gmail.com>
2018-05-22 15:30:17 +0200

    (WIP) Move style into theme components

---
+++ src/themes/Dark.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <slot/>
+  </div>
+</template>
+<!-- Universal styles -->
+<style lang="scss">
+// Import theme variables first and overrides last, do not change the order
+// Change to Darkly for dark theme and Flatly for light theme
+// @import "~bulmaswatch/flatly/variables";
+@import '~bulmaswatch/darkly/variables';
+@import '~bulma/sass/utilities/_all';
+
+/* --- Begin custom variables --- (https://buefy.github.io/#/documentation/customization) */
+
+// Set your colors
+// $primary: #8c67ef;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+  'white': ($white, $black),
+  'black': ($black, $white),
+  'light': ($light, $light-invert),
+  'dark': ($dark, $dark-invert),
+  'primary': ($primary, $primary-invert),
+  'info': ($info, $info-invert),
+  'success': ($success, $success-invert),
+  'warning': ($warning, $warning-invert),
+  'danger': ($danger, $danger-invert),
+  'twitter': ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+$tablet: 768px;
+
+/* --- End custom variables --- */
+
+@import '~bulma';
+@import '~buefy/src/scss/buefy';
+// @import "~bulmaswatch/flatly/overrides";
+@import '~bulmaswatch/darkly/overrides';
+
+.load-overlay{
+  position: relative;
+  min-height:100px;
+}
+.no-overflow {
+  overflow: initial;
+}
+</style>
