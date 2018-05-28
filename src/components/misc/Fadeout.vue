819066de896de42a091648baca4c80bc41f974a8
Dacrol <jonas.ahrens@gmail.com>
2018-05-28 15:06:54 +0200

    Change from filtering to computed

--- src/components/misc/Fadeout.vue
+++ src/components/misc/Fadeout.vue
@@ -1,34 +1,9 @@
 <template>
-  <div>
-
-  </div>
+  <div/>
 </template>
 
 <script>
 export default {
-  filters: {
-    truncate: function (value) {
-      const truncateChars = 250
-      const fadeChars = 25
-      let truncated =
-        value.length > truncateChars ? value.substring(0, truncateChars) : value
-      if (truncated.length >= truncateChars) {
-        // truncated = truncated + '...'
-        // truncateChars = truncated.length
-        let faded = Array.prototype.map.call(
-          truncated.substring(truncateChars - fadeChars, truncateChars),
-          (char, index) => {
-            let x = index / fadeChars
-            let quickMaffs = 1 - 0.12 * x - 0.88 * x ** 2
-            return `<span style="opacity: ${quickMaffs};">${char}</span>`
-          }
-        )
-        truncated =
-          truncated.substring(0, truncateChars - fadeChars) + faded.join('')
-      }
-      return truncated
-    }
-  },
   props: {
     fadeChars: {
       default: 25,
@@ -41,6 +16,31 @@ export default {
     text: {
       default: '',
       type: String
+    }
+  },
+  computed: {
+    truncated: function () {
+      const truncateChars = this.truncateChars
+      const fadeChars = this.fadeChars
+      const value = this.text
+      let result = []
+
+      if (value.length > truncateChars) {
+        result[0] = [value.substring(0, truncateChars - fadeChars), 1]
+        let faded = Array.prototype.map.call(
+          value.substring(truncateChars - fadeChars, truncateChars),
+          (char, index) => {
+            let x = index / fadeChars
+            let quickMaffs = 1 - 0.12 * x - 0.88 * x ** 2
+            // return `<span style="opacity: ${quickMaffs};">${char}</span>`
+            return [char, quickMaffs]
+          }
+        )
+        result = result.concat(faded)
+      } else {
+        result = [[value, 1]]
+      }
+      return result
     }
   }
 }
