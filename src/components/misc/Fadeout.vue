8d858abbd397e4e59f5ca9f5a16e330fb446f2ba
Dacrol <jonas.ahrens@gmail.com>
2018-05-28 12:51:28 +0200

    Add fadeout filter code & props

--- src/components/misc/Fadeout.vue
+++ src/components/misc/Fadeout.vue
@@ -1,13 +1,50 @@
 <template>
+  <div>
 
+  </div>
 </template>
 
 <script>
 export default {
-
+  filters: {
+    truncate: function (value) {
+      const truncateChars = this.truncateChars
+      const fadeChars = this.fadeChars
+      let truncated =
+        value.length > truncateChars ? value.substring(0, truncateChars) : value
+      if (truncated.length >= truncateChars) {
+        // truncated = truncated + '...'
+        // truncateChars = truncated.length
+        let faded = Array.prototype.map.call(
+          truncated.substring(truncateChars - fadeChars, truncateChars),
+          (char, index) => {
+            let x = index / fadeChars
+            let quickMaffs = 1 - 0.12 * x - 0.88 * x ** 2
+            return `<span style="opacity: ${quickMaffs};">${char}</span>`
+          }
+        )
+        truncated =
+          truncated.substring(0, truncateChars - fadeChars) + faded.join('')
+      }
+      return truncated
+    }
+  },
+  props: {
+    fadeChars: {
+      default: 25,
+      type: Number
+    },
+    truncateChars: {
+      default: 250,
+      type: Number
+    },
+    text: {
+      default: '',
+      type: String
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
